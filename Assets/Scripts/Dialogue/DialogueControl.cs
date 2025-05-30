770ddb28f71feb2f41cd4df9195a2940a5baa940
Arggusx <dougclasher9@gmail.com>
2025-05-29 22:10:20 -0300

    Adiciona animações, diálogos e inteligência ao NPC

---
+++ Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [System.Serializable]
+    public enum idiom
+    {
+        pt,
+        ing,
+        spa
+    }
+
+    public idiom language;
+
+    [Header("Components")]
+    public GameObject dialogueObj; // Janela de diálogo
+    public Image profileSprite; // Imagem do NPC
+    public Text speechText; // Texto de diálogo do NPC
+    public Text actorNameText; // Nome do NPC
+
+    [Header("Settings")]
+    public float typingSpeed; // Velocidade da fala
+
+    // Variáveis de controle
+    [HideInInspector] public bool isShowing; // Para saber  se a janela está aberta
+    private int index; // Par saber quantos textos tem em um diálogo
+    private string[] sentences;
+
+    public static DialogueControl instance;
+
+    private void Awake() // Na hierarquia de scripts, 'Awake' é o primeiro a ser chamado
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator TypeSentence() // Coroutine que escreve uma frase letra por letra, com um intervalo entre cada letra
+    {
+        foreach (char letter in sentences[index].ToCharArray()) // Para cada caractere (letter) da frase atual (convertemos a string em array de caracteres)
+        {
+            speechText.text += letter; // Adiciona a letra atual ao texto exibido na interface
+            yield return new WaitForSeconds(typingSpeed); // Espera por um curto intervalo de tempo antes de adicionar a próxima letra
+        }
+    }
+
+    public void NextSentence() // Pula para a próxima frase
+    {
+        if (speechText.text == sentences[index]) // Se a frase apareceu por completo
+        {
+            if (index == sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else // Quando termina os textos
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+                sentences = null;
+                isShowing = false;
+            }
+        }
+    }
+
+    public void Speech(string[] txt) // Chama a fala do NPC
+    {
+        if (!isShowing) // Para enquanto uma fala continuar, não executar o 'Speech' novamente
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+
+}
---
+++ Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+
+    public float dialogueRange; // Alcance em que o jogador pode interagir com o NPC   
+    public LayerMask playerLayer; // Camada que representa o jogador (para detectar colisões apenas com o jogador)
+
+    public DialogueSettings dialogue; // Referência para as configurações de diálogo deste NPC
+
+    bool playerHit; // Flag que indica se o jogador está dentro da área de interação
+
+    private List<string> sentences = new List<string>(); // Lista que armazena as frases do diálogo em formato de string
+
+    void Start()
+    {
+        GetNPCInfo();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerHit)  // Verifica se o jogador apertou a tecla 'E' e está perto do NPC para iniciar o diálogo
+        {
+            Debug.Log("Cadê o diálogo");
+            // Chama o método que inicia o diálogo, passando as frases como array
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    // Método para coletar as frases em português do diálogo do NPC e armazená-las na lista
+    void GetNPCInfo()
+    {
+        for (int i = 0; i < dialogue.dialogues.Count; i++)
+        {
+
+            switch (DialogueControl.instance.language)
+            {
+                case DialogueControl.idiom.pt:
+                    // Adiciona cada frase em português à lista de sentenças
+                    sentences.Add(dialogue.dialogues[i].sentences.portuguese);
+                    break;
+                case DialogueControl.idiom.ing:
+                    // Adiciona cada frase em português à lista de sentenças
+                    sentences.Add(dialogue.dialogues[i].sentences.english);
+                    break;
+                case DialogueControl.idiom.spa:
+                    // Adiciona cada frase em português à lista de sentenças
+                    sentences.Add(dialogue.dialogues[i].sentences.spanish);
+                    break;
+            }
+
+            // Adiciona cada frase em português à lista de sentenças
+            sentences.Add(dialogue.dialogues[i].sentences.portuguese);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    // Verifica se há colisão do jogador dentro da área de diálogo
+    void ShowDialogue()
+    {
+        // Cria o colisor ao redor do NPC para detectar o jogador
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+
+        // Se detectar o jogador, define playerHit como true
+        if (hit != null)
+        {
+            playerHit = true;
+        }
+        else
+        {
+            playerHit = false;
+        }
+    }
+
+    // Desenha um círculo visível no editor para mostrar o alcance do diálogo
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
